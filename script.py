587a6bd2069414877fe4e05d4031d423ecaf6c2c
Kevin Jimenez <kjimenezp1@est.ups.edu.ec>
2021-01-01 19:44:27 -0500

    Agregado datos del proyecto

---
+++ script.py
@@ -0,0 +1,12 @@
+# Script de prueba para Git
+# Autor: Kevin Jimenez <kjimenezp1@ups.edu.ec>
+# Fecha: 01-01-2020
+# Versión: 1.0.0
+
+def main():
+    nombre = input("Cual es tu nombre?")
+    print("Hola", nombre, "Bienvenido a Git")
+
+if __name__ == "__main__":
+    main()
+
